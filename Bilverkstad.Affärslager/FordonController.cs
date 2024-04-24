9da27b2f2b1fa8a712d462396f130ce58e1b2cb1
ABDULLA MEHDI <s2305430@student.hb.se>
2024-04-24 15:55:05 +0200

    ok

--- Bilverkstad.Affärslager/FordonController.cs
+++ Bilverkstad.Affärslager/FordonController.cs
@@ -8,58 +8,19 @@ using System.Threading.Tasks;
 
 namespace Bilverkstad.Affärslager
 {
-        public class FordonController
-        {
+    public class FordonController
+    {
         KundController kundController = new KundController();
-            public FordonController() { }
+        public FordonController() { }
 
-            public IList<Fordon> GetFordon()
-            {
-                using (UnitOfWork unitOfWork = new UnitOfWork())
-                {
-                    return (IList<Fordon>)unitOfWork.Fordon.GetAll().ToList();
-                }
-
-            }
-
-            public Fordon GetOneFordon(string reg)
-            {
-                using (UnitOfWork unitOfWork = new UnitOfWork())
-                {
-                    return unitOfWork.Fordon.FindStringID(reg);
-                }
-            }
-
-            {
-                using (UnitOfWork unitOfWork = new UnitOfWork())
-                {
-                    unitOfWork.Fordon.Add(fordon);
-                    unitOfWork.SaveChanges();
-                }
-        }
-
-        public void AddFordontoKund(Kund kund, Fordon fordon)
+        public IList<Fordon> GetFordon()
         {
-            kund.Fordon.Add(fordon);
-            fordon.Kund = kund;
-            }
-
+            using (UnitOfWork unitOfWork = new UnitOfWork())
             {
-                using (UnitOfWork uow = new UnitOfWork())
-                {
-                    Fordon gammaltFordon = uow.Fordon.FindStringID(fordon.RegNr);
-                    uow.Fordon.Delete(gammaltFordon);
-                    uow.SaveChanges();
-                }
-            }
-            {
-                using (UnitOfWork uow = new UnitOfWork())
-                {
-                    Fordon benfintligtFordon = uow.Fordon.FindStringID(fordon.RegNr);
-                    uow.Fordon.Update(benfintligtFordon, fordon);
-                    uow.SaveChanges();
-                }
+                return (IList<Fordon>)unitOfWork.Fordon.GetAll().ToList();
             }
 
         }
-}
+
+    }
+}   
