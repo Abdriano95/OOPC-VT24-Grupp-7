cd9d1fde7578d6cfa2d383863b62b72b9fdd41b9
joakimolssonn <s2306157@student.hb.se>
2024-04-26 15:41:07 +0200

    upd

--- Bilverkstad.Presentationslager/LäggTillReparationWindow.xaml.cs
+++ Bilverkstad.Presentationslager/LäggTillReparationWindow.xaml.cs
@@ -21,24 +21,65 @@ namespace Bilverkstad.Presentationslager
     /// </summary>
     public partial class LäggTillReparationWindow : Window
     {
-        ReparationController controller = new ReparationController();
+        ReparationController reparationcontroller = new ReparationController();
+        ReservdelController reservdelcontroller = new ReservdelController();
+        Reservdel reservdel = new Reservdel();
+        List<int> artikelnummerList;
         public LäggTillReparationWindow()
         {
             InitializeComponent();
             FillComboBoxWithEnums();
+            LoadArtikelnummer();
+            ReservdelController reservdelController = new ReservdelController();
+            Reservdel.ItemsSource = reservdelController.GetReservdel();
         }
         public void AddReparation_Click(object sender, RoutedEventArgs e)
         {
+            
+            
             var reparation = new Reparation
             {
                 Åtgärd = txtÅtgärd.Text,
-                Reparationsstatus = (Reparationsstatus)cbReparationsstatus.SelectedItem
-                //Bokning ID
+                Reparationsstatus = (Reparationsstatus)cbReparationsstatus.SelectedItem,
+                
                 // Reservdelar
                 // Mekaniker ID och kopplad till åtgärd
+                //Bokning ID
             };
-            controller.AddReparation(reparation);
+            reparation.Reservdelar.Add(reservdel);
+            reparationcontroller.UpdateReparation(reparation);
+            reparationcontroller.AddReparation(reparation);
+            
+            ResetForm();
         }
+        private void LoadArtikelnummer()
+        {
+            // Hämta alla artikelnummer för reservdelarna
+            artikelnummerList = new List<int>(reservdelcontroller.GetArtikelnummer());
+
+            // Fyll ComboBoxen med artikelnummer
+            foreach (int artikelnummer in artikelnummerList)
+            {
+                cbArtikelnummer.Items.Add(artikelnummer);
+            }
+        }
+        private void ResetForm()
+        {
+            // Återställ alla inmatningsfält och ComboBoxen
+            txtÅtgärd.Text = "";
+            cbReparationsstatus.SelectedIndex = -1;
+            cbArtikelnummer.SelectedIndex = -1;
+        }
+        private void cbArtikelnummer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int inmatning = cbArtikelnummer.SelectedIndex;  
+            if (cbArtikelnummer.SelectedItem != null)
+            {
+                reservdel = reservdelcontroller.GetOneReservdel(inmatning);
+                // Använd den valda artikelnumret här, till exempel spara det i en variabel eller använd det på annat sätt
+            }
+        }
+
         private void cbReparationsstatus_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (cbReparationsstatus.SelectedItem != null)
