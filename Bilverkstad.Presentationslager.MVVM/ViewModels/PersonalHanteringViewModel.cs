2d3d53230fb4f5b670b7e3b17635cc4fce7f6a3a
joakimolssonn <s2306157@student.hb.se>
2024-05-16 10:00:46 +0200

    class created

---
+++ Bilverkstad.Presentationslager.MVVM/ViewModels/PersonalHanteringViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilverkstad.Presentationslager.MVVM.ViewModels
+{
+    public class PersonalHanteringViewModel : BaseViewModel
+    {
+
+    }
+}
